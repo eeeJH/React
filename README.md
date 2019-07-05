728cc1310dde729bb655b6464e8ddc267a4f8e29
juhyeon lee <esloven3@gmail.com>
2019-07-05 13:00:08 +0900

    Update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # React
+
+'''*Nomad Coders'''
+
+Youtube 보고 React 따라하기
+
+- React를 장고, 루비온레일즈, nodejs랑 섞어서 씀
+
+
+1. Movie App
---
+++ movie-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Movie from './Movie';
+
+function App() {
+  return (
+    //jsx 리액트 컴포넌트를 만들때 사용하는 언어.
+    <div className="App">
+      <Movie />
+    </div>
+  );
+}
+
+export default App;
