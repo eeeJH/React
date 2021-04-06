07f041afff65ef852ebbbc9d37693703df70ac7d
leejuhyeon <esloven3@gmail.com>
2021-04-06 22:48:53 +0900

    Update

--- basic/webservice/src/Movie.js
+++ basic/webservice/src/Movie.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
@@ -23,13 +22,29 @@ import './Movie.css';
 // }
 
 
-function Movie({title, poster }) {
+function Movie({title, poster, genres, synopsis }) {
     return (
-        <div>
-            <MoviePoster poster={poster} />
-            <h1>
-                {title}
-            </h1>
+        <div className='Movie'>
+
+            <div className='Movie__Colm'>
+                <MoviePoster poster={poster} alt={ title }/>
+            </div>
+
+            <div className='Movie__Colm'>
+
+                <h1>
+                    {title}
+                </h1>
+
+                <div className='Movie__Genr'>
+                    {genres.map((genres, index) => <MovieGenre genres={genres} key={index}/>)}
+                </div>
+
+                <p className='Movie__Syno'>
+                    {synopsis}
+                </p>
+            </div>
+            
         </div>
     );
 }
@@ -51,22 +66,36 @@ function Movie({title, poster }) {
 // 
 // return 만 한다.
 // render 과정이 없음
-function MoviePoster({ poster }) {
+function MoviePoster({ poster, alt }) {
     return (
-        <img src={ poster }/>
+        <img src={poster} alt={alt} title={ alt } className='Movie__Poster' />
     );
 }
 
+function MovieGenre({ genres}) {
+            
+    return (
+        <span className='Movie__Genr'>
+            {genres}
+        </span>
+    );
+}
 
 Movie.prototype = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
 
 MoviePoster.prototype = {
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
 }
 
+MovieGenre.prototype = {
+    genres: PropTypes.string.isRequired
+}
 
 export default Movie;
